cc9a2880b8d2bee26dbaaf4549053d3b52d670ac
Kashyap Aghera <kaagh.10@gmail.com>
2020-10-20 20:18:34 -0400

    modified tested files

--- Develop/app.js
+++ Develop/app.js
@@ -18,7 +18,7 @@ function addMember() {
     },
     {
         type: "checkbox",
-        message: "Select team member's role",
+        message: "Enter team member's role",
         choices: [
             "Engineer",
             "Intern",
@@ -31,7 +31,7 @@ function addMember() {
         name: "id"
     },
     {
-        message: "Enter team member's email address",
+        message: "Enter team member's email",
         name: "email"
     }])
         .then(function ({ name, role, id, email }) {
@@ -41,7 +41,7 @@ function addMember() {
             } else if (role === "Intern") {
                 roleInfo = "school name";
             } else {
-                roleInfo = "office phone number";
+                roleInfo = "phone number";
             }
             inquirer.prompt([{
                 message: `Enter team member's ${roleInfo}`,
@@ -77,30 +77,6 @@ function addMember() {
 
                 });
         });
-}
-
-function teamHtml() {
-    const html = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <title>Team Profile</title>
-    </head>
-    <body>
-        <nav class="navbar navbar-dark bg-dark mb-5">
-            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
-        </nav>
-        <div class="container">
-            <div class="row">`;
-    fs.writeFile("./output/team.html", html, function (err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-    
 }
 
 function addHtml(member) {
@@ -155,6 +131,30 @@ function addHtml(member) {
             return resolve();
         });
     });
+}
+
+function teamHtml() {
+  const html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <title>Team Profile</title>
+  </head>
+  <body>
+      <nav class="navbar navbar-dark bg-dark mb-5">
+          <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+      </nav>
+      <div class="container">
+          <div class="row">`;
+  fs.writeFile("./output/team.html", html, function (err) {
+      if (err) {
+          console.log(err);
+      }
+  });
+  
 }
 
 function endHtml() {
